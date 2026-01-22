c97bc7abb46976decf191d13d113ac0cdffd6dfc
esteban572 <esteban.j.ibarra@gmail.com>
2026-01-21 22:35:22 -0600

    Restore original card photos and names in initialCards array

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-plus/cards/lake.jpg"
+    name: "Val Thorens",
+    link: "./images/1-photo-by-moritz-feldmann-from-pexels.jpg"
   },
   {
-    name: "Parque Nacional Torres del Paine",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-plus/cards/yosemite.jpg"
+    name: "Restaurant terrace",
+    link: "./images/2-photo-by-ceiline-from-pexels.jpg"
   },
   {
-    name: "Camino de los Dioses",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-plus/cards/chicago.jpg"
+    name: "An outdoor cafe",
+    link: "./images/3-photo-by-tubanur-dogan-from-pexels.jpg"
   },
   {
-    name: "Valle de la Luna",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-plus/cards/baikal.jpg"
+    name: "A very long bridge, over the forest",
+    link: "./images/4-photo-by-maurice-laschet-from-pexels.jpg"
   },
   {
-    name: "Glaciar Perito Moreno",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-plus/cards/kamchatka.jpg"
+    name: "Tunnel with morning light",
+    link: "./images/5-photo-by-van-anh-nguyen-from-pexels.jpg"
   },
   {
-    name: "Parque Nacional Los Glaciares",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-plus/cards/karjala.jpg"
+    name: "Mountain house",
+    link: "./images/6-photo-by-moritz-feldmann-from-pexels.jpg"
   }
 ];
 
